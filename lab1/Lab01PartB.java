76fd1621ec5e496b3cee47ffb4c7380da7d23e92
ShamodGeevinda <e18397@eng.pdn.ac.lk>
2021-11-19 16:43:29 +0530

    lab 1 part B done

--- lab1/Lab01PartB.java
+++ lab1/Lab01PartB.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lab01PartB {
     
     public static void main(String args[]) {
@@ -7,9 +9,25 @@ public class Lab01PartB {
     
     public static void findEvenOrOdd(int number){
         //Implement B a
+		if (number % 2 == 0){
+			System.out.println("Even Number");
+		}else{
+			System.out.println("Odd Number");
+		}
         
         
         //Implement B b
+		Scanner scan = new Scanner(System.in);
+		int input = scan.nextInt();
+		scan.close();
+		if (input % 2 == 0){ // printing next 5 even numbers
+			for(int i =0; i<5;i++)
+				System.out.println(input+2*(i+1));
+		}else{
+			for(int i =0; i<3;i++) // printing next 3 odd numbers
+				System.out.println(input+2*(i+1));
+		}
+		
     }
     
 }
