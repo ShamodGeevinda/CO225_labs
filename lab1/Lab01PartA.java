be1d1a5bd10c841c6d313b1467962c9a023422e2
ShamodGeevinda <e18397@eng.pdn.ac.lk>
2021-11-19 15:46:32 +0530

    lab1 part A done

--- lab1/Lab01PartA.java
+++ lab1/Lab01PartA.java
@@ -6,7 +6,48 @@ public class Lab01PartA {
     }
     
     public static void showGrade(int marks){
-        //Implement A
-    }
+        
+		int mark = marks; // storinhg original marks value
+		switch(marks/10) {
+			
+			// 100 - 80
+			case 10:
+			case 9:
+			case 8:
+			System.out.println("A");
+			break;
+			
+			// 79 - 70
+			case 7:
+			System.out.println("B");
+			break;
+			
+			case 6:
+			if (mark>=65){
+				System.out.println("B"); // 69 - 65
+			}else{
+				System.out.println("C"); // 64 - 60
+			}
+			break;
+			
+			case 5:
+			if (mark>=55){
+				System.out.println("C"); // 59 - 55 
+			}else{
+				System.out.println("D"); // 54 - 50
+			}
+			break;
+			
+			// 49 - 0
+			case 4:
+			case 3:
+			case 2:
+			case 1:
+			case 0:
+			System.out.println("F");
+			break;
+				
+		}
+	}
     
 }
