4faf1b0b3be8be0d566ff3cec644db54261dc326
ShamodGeevinda <e18397@eng.pdn.ac.lk>
2021-11-19 19:14:26 +0530

    lab 1 part C done

--- lab1/Lab01PartC.java
+++ lab1/Lab01PartC.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Lab01PartC {
     
     public static void main(String args[]) {
@@ -9,9 +10,36 @@ public class Lab01PartC {
         int monthValue =0;
         
         //Implement C a
+		String[] months = {"JANUARY","FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"};
+        for (int i=0; i<12; i++){ // loop to check the value of the month
+			if(month==months[i]){
+				monthValue = i+1;
+			}
+		}
+        System.out.println(monthValue);
         
-        
+		
         //Implement C b
-    }
+		
+		// getting month by user
+		Scanner scan = new Scanner(System.in);
+		//scan.nextLine();
+		String userMonth = scan.nextLine();
+		scan.close();
+		
+		monthValue =0;
+		
+		for (int i=0; i<12; i++){ // loop to check the value of the month
+			if(userMonth.equals(months[i])){
+				monthValue = i+1;
+			}
+		}
+		
+		monthValue += 1;
+		while(monthValue <= 12){ // loop to print upccomming value
+			System.out.println(months[monthValue-1]);
+			monthValue++;
+		}
+	}
     
 }
