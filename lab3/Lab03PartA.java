bd3ea2ae853d0315fff7cd0d2f46f00649fdf548
ShamodGeevinda <e18397@eng.pdn.ac.lk>
2021-12-17 15:04:30 +0530

    lab 3 initia;

---
+++ lab3/Lab03PartA.java
@@ -0,0 +1,9 @@
+public class Lab03PartA
+{
+    public static void main(String args[])
+    {
+		//Implemnt Lab03PartA
+	}
+	
+}
+	
---
+++ lab3/Lab03PartB.java
@@ -0,0 +1,19 @@
+public class Lab03PartB
+{
+    public static void main(String args[])
+    {
+		//Implemnt Lab03PartB
+	}
+	
+	public void findAdjacentsByArray()
+	{
+		//Implemnt Lab03PartB a 
+	}
+	
+	public void findAdjacentsByMap()
+	{
+		//Implemnt Lab03PartB b
+	}
+	
+}
+	
