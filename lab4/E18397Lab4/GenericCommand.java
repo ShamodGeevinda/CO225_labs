ba6ea1dd6f5a7a48ee64394b9ec7d84ad51a28e7
ShamodGeevinda <e18397@eng.pdn.ac.lk>
2022-01-02 06:39:09 +0530

    submission zip file added

---
+++ lab4/E18397Lab4/GenericCommand.java
@@ -0,0 +1,22 @@
+/*******************************************************************
+                 Skeleton code 
+********************************************************************/
+
+public class GenericCommand { 
+    /* this is the generic command 
+     * your commands should reimplement this
+     */
+    public static int n =0; // variable to store command count
+    public void handleCommand(String [] args) { 
+	System.out.println("Command not implemented!");
+    }
+
+    public void someThingWrong() { 
+	System.out.println("Something Wrong in code!!!");
+	System.exit(-1);
+    }
+
+    
+    
+}
+
