5a3d1ff73ab313f3949d5220d822a687409152f4
ShamodGeevinda <e18397@eng.pdn.ac.lk>
2022-01-22 15:00:17 +0530

    output file printing done

--- lab5/Lab05.java
+++ lab5/Lab05.java
@@ -2,22 +2,22 @@ import java.math.*;
 import java.util.Random; // for random 
 import java.io.*; // for file IO
 
- 
 public class Lab05 {
     public static void main(String[] args) {
     final int LEC_COUNT = 45;
     final int STUDENT_COUNT = 61;
     
-    Student[] stuArray = new Student[STUDENT_COUNT];
+    // Student class array to hold data
+    Student[] stuArray = new Student[STUDENT_COUNT]; 
 
     BufferedReader descriptor = null; 
 	String line = null; 
-	//String [] names = new String[61]; // 61 names 
+	
 	try { 
 	    descriptor = new BufferedReader(
-			    new FileReader("co225-classlist.txt"));
+		new FileReader("co225-classlist.txt"));
 
-	    for(int i=0; i < stuArray.length; i++) // assume file has 61 lines 
+	    for(int i=0; i < stuArray.length; i++) 
 		stuArray[i] = new Student (descriptor.readLine(), LEC_COUNT); 
 
 	    descriptor.close(); 
@@ -28,16 +28,54 @@ public class Lab05 {
 	    System.out.println("Bad things happen, what do you do!" + e);
 	    return; 
 	}
+   
 
-	// we will run only if no exceptions were thrown 
-	for(int i=0; i < stuArray.length; i++) 
+	
+
+	// we will run only if no exceptions were thrown
+    try {
+        PrintWriter writer = new PrintWriter("co225-classlist.txt");
+        writer.print("");
+        writer.close();
+        PrintWriter writer2 = new PrintWriter("co225-classlist-ineligible.txt");
+        writer2.print("");
+        writer2.close();
+    } catch (Exception e) {
+        //TODO: handle exception
+    }
+    
+	for(int i=0; i < stuArray.length; i++) {
         if(stuArray[i].getAttendancePercent(LEC_COUNT)<80){
-            //System.out.println(stuArray[i].getName()+" " +i + " " +stuArray[i].getAttendancePercent(LEC_COUNT)+ " " + stuArray[i].getAttendance());
+            System.out.println(stuArray[i].getName());
+            try {
+                BufferedWriter bw = new BufferedWriter(new FileWriter(new File("co225-classlist-ineligible.txt"), true));
+                BufferedWriter bw2 = new BufferedWriter(new FileWriter(new File("co225-classlist.txt"), true));
+                bw.write(stuArray[i].getName());
+                bw2.write(stuArray[i].getName()+"-ineligible");
+                bw.newLine();
+                bw.close();
+                bw2.newLine();
+                bw2.close();
+		    } 
+            catch (Exception e) {
+		    }
+        }else{
+            try {
+                BufferedWriter bw2 = new BufferedWriter(new FileWriter(new File("co225-classlist.txt"), true));
+                bw2.write(stuArray[i].getName());
+                bw2.newLine();
+                bw2.close();
+            } catch (Exception e) {
+                //TODO: handle exception
+            }
+            
+
         }
      
 
     }
-
+   
+    }
 
 }
 
