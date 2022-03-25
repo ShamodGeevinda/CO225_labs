ee86d7458f59d314af8bbc59c80c627fb618f8e1
ShamodGeevinda <e18397@eng.pdn.ac.lk>
2022-03-25 11:42:39 +0530

    working fine

--- lab8/Matrix.java
+++ lab8/Matrix.java
@@ -1,13 +1,37 @@
+// matrix class
 public class Matrix extends Thread { 
 
+	// a, b input matrices
+	// c output matrix
     private static int [][] a; 
     private static int [][] b; 
     private static int [][] c; 
 
-    /* You might need other variables as well */
+	private static Matrix threads[][];
 
-    public Matrix() { // need to change this, might need some information 
+    /* You might need other variables as well */
+	/* i1 : To store i
+	   j1 : To store j
+	   z : To store z1 */
+	private int i1, j1, z;
+
+    public Matrix(int i1, int j1, int z, int[][]a1, int[][]b1) { // need to change this, might need some information 
+    	this.i1 = i1;
+    	this.j1 = j1;
+    	this.z = z;
+    	a = a1;
+    	b = b1;
+	}
+
+	/* Run method will calculate each element of the matrix c */
+	public void run() {
+    	int  k, s;
+    		for(s=0, k=0; k<z; k++) 
+    		    s += a[i1][k] * b[k][j1];
+    		c[i1][j1] = s;
+    	    
     }
+
 
     public static int [][] multiply(int [][] a, int [][] b) {
 
@@ -28,15 +52,29 @@ public class Matrix extends Thread {
 	    return null;
 	}
 
-	int [][] c = new int [x][y]; 
-	int i, j, k, s; 
+	/* Create number of threads equal to the number of elements of the output matrix */
+	threads = new Matrix[x][y];
+	
+	c = new int [x][y];
 
-	for(i=0; i<x; i++) 
-	    for(j=0; j<y; j++) {
-		for(s=0, k=0; k<z1; k++) 
-		    s += a[i][k] * b[k][j];
-		c[i][j] = s;
-	    }
+	/* Start the threads */
+	for(int i = 0;i < x;i++) {
+		for(int j = 0;j < y;j++) {
+			threads[i][j] = new Matrix(i, j, z1, a, b);
+			threads[i][j].start(); /* run method is called */
+		}
+	}
+	
+	/* Wait for finishing all the threads */
+	for(int i = 0;i < x;i++) {
+		for(int j = 0;j < y;j++) {
+			try {
+				threads[i][j].join();
+			} catch (InterruptedException e) {
+				System.out.println("InterruptedException is occurred");
+			}
+		}
+	}
 
 	return c; 
     }
